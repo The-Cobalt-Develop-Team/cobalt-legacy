50058848c0cbce895bc88cd592c68bc969b227ff
AndyShen2006 <andyshen2006@foxmail.com>
2023-04-05 21:27:37 +0000

    Remove Lithium temporary: Because it have't completed yet.

--- src/Common/Lithium/test/build.sh
+++
@@ -1,3 +0,0 @@
-# clang++ test.cpp ../Logger/src/Logger.cpp ../Logger/src/LoggerNode.cpp -o test.out -Wall -fsanitize=address -fuse-ld=lld
-g++ test.cpp ../Logger/src/Logger.cpp ../Logger/src/LoggerNode.cpp -o test.out -Wall -fsanitize=address
-./test.out
